868d96912cc7c9f2675c6804318838076f0efc44
Andrew Bloom <blooma116@gmail.com>
2018-03-10 20:48:31 -0800

    adding contact info page

--- mysite/webapp/views.py
+++ mysite/webapp/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'webapp/index.html')
+
+
+def contact(request):
+    return render(request,
+                  'webapp/basic.html',
+                  {'info': [
+                        'I\'d love to hear from you!',
+                        'Send me an email and say hi',
+                        'blooma116 -at- gmail',
+                  ]})
