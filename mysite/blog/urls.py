bdb6910b47069c5e8ee8ee51644f397ca2e0b541
Andrew Bloom <blooma116@gmail.com>
2018-03-10 23:12:46 -0800

    finished implementing blog functionality

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 
 urlpatterns = [
     path('', ListView.as_view(queryset=Post.objects.all()
-                              .order_by("-date")[:10],
-                              template_name="blog/blog.html")),
+                              .order_by('-date')[:10],
+                              template_name='blog/blog.html')),
+    # regex: primary key of one or more digits
+    re_path(r'^(?P<pk>\d+)',
+            DetailView.as_view(model=Post,
+                               template_name='blog/post.html')),
 ]
--- mysite/webapp/urls.py
+++ mysite/webapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('webapp/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
 ]
