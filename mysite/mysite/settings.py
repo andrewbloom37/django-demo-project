4971e1862366ef8c0dc975d6776b52831962cff5
Andrew Bloom <blooma116@gmail.com>
2018-03-13 01:12:27 -0700

    configuring staticfiles

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,6 +133,10 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
